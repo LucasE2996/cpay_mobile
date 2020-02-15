62326dfe9693269d2da2782d6a93605524aa6931
Patric Pereira <patric.pereirafranca@gmail.com>
2020-02-15 12:21:58 -0300

    creating page Login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,12 +1,49 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useState } from 'react';
+import {
+  Keyboard,
+  KeyboardAvoidingView,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  SafeAreaView,
+  View,
+} from 'react-native';
 
 import styles from './styles';
 
 export default function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
-    <View style={styles.container}>
-      <Text>Hello World</Text>
-    </View>
+    <KeyboardAvoidingView behavior="padding" style={{ flex: 1 }} enabled>
+      <SafeAreaView style={styles.container}>
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <View>
+            <TextInput
+              keyboardType="email-address"
+              autoCorrect={false}
+              autoCapitalize="none"
+              placeholder="Digite seu email"
+              value={email}
+              onChangeText={setEmail}
+            />
+            <TextInput
+              secureTextEntry
+              keyboardType="default"
+              autoCorrect={false}
+              autoCapitalize="none"
+              placeholder="Digite sua senha"
+              value={password}
+              onChangeText={setPassword}
+            />
+            <TouchableOpacity onPress={() => {}}>
+              <Text>Acessar</Text>
+            </TouchableOpacity>
+          </View>
+        </TouchableWithoutFeedback>
+      </SafeAreaView>
+    </KeyboardAvoidingView>
   );
 }
